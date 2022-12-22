2fc8f6b65d6bec3e1d4ebeae38fa32dcc1603bb1
Gus Oliver <gustavooliver.souza@gmail.com>
2022-12-21 23:31:14 -0300

    ex032.1

---
+++ ex032/script1.js
@@ -0,0 +1,34 @@
+let screen = document.getElementById("screen");
+let pincel = screen.getContext("2d");
+
+function step1(){
+    setInterval(step2,500);
+    pincel.moveTo(0,0);
+    pincel.lineTo(75,100)
+    pincel.lineWidth = 2;
+    pincel.strokeStyle = "rgb(108, 66, 175)";
+    pincel.stroke();
+}
+function step2(){
+    setInterval(step3,500);
+    pincel.moveTo(75,100);
+    pincel.lineTo(150,0)
+    pincel.lineWidth = 1;
+    pincel.strokeStyle = "rgb(77, 42, 143)";
+    pincel.stroke();
+}
+function step3(){
+    setInterval(step4,500);
+    pincel.moveTo(150,0);
+    pincel.lineTo(235,100)
+    pincel.lineWidth = 2;
+    pincel.strokeStyle = "rgb(108, 66, 175)";
+    pincel.stroke();
+}
+function step4(){
+    pincel.moveTo(235,100);
+    pincel.lineTo(300,0)
+    pincel.lineWidth = 1;
+    pincel.strokeStyle = "rgb(77, 42, 143)";
+    pincel.stroke();
+}
